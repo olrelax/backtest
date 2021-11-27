d87ff6e1d215812cd5fa16f0464e34ed651b3ed6
oleg <olrelax@gmail.com>
2021-11-27 15:20:56 +0300

    2

--- opt_trade.py
+++ opt_trade.py
@@ -16,19 +16,6 @@ if long_shift <= 0 < short_shift:
     exit('wrong shift values')
 
 
-def get_closest(df, val, put_or_call='put'):
-    min_delta, premium, strike = 1000000., 0., 0
-    for index, row in df.iterrows():
-        opt_type = row['type']
-        if not opt_type == put_or_call:
-            continue
-        row_val = row['strike_price']
-        delta = abs(row_val - val)
-        if delta < min_delta:
-            min_delta = delta
-            strike = row_val
-            premium = row['option_price']
-    return strike, premium
 
 
 def bp(doit):
