1d809cb8de670934a2fde3371596a6a43594f275
oleg <olrelax@gmail.com>
2021-12-04 20:18:03 +0300

    7

--- au.py
+++ au.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from datetime import datetime
+def d2s(dtm,fmt='%Y-%m-%d'):
+    return datetime.strftime(dtm,fmt)
+def s2d(text,fmt='%Y-%m-%d'):
+    return datetime.strptime(text,fmt)
 
 dtm_fmt_Y_m_d = '%Y-%m-%d'
 def prn(text, color=''):
@@ -20,29 +24,45 @@ def prn(text, color=''):
     else:
         print(text)
 
-
-def get_opt(df_a, quote_date,expiration,strike,t,up=True,max_delta=1,expiration_mode_search='n'):
-    qd = quote_date     # datetime.strptime(quote_date,dtm_fmt_Y_m_d)
-    exp = expiration    #  datetime.strptime(expiration,dtm_fmt_Y_m_d)
-    df = df_a.loc[df_a['option_type'] == t].loc[df_a['quote_date'] == qd].copy()
-    if not expiration_mode_search == 'e':    # not exact expiration date
+def get_opt(df_a, quote_date,expiration,strike,exp_mode_search,strike_round_mode,tp='P',max_delta=1):
+    qd = quote_date
+    exp = expiration
+    if strike_round_mode not in ('up','down','exact'):
+        exit('wrong strike_round_mode')
+    if exp_mode_search not in ('closest_next','closest_previous','exact'):
+        exit('wrong exp_mode_search')
+    df = df_a.loc[df_a['option_type'] == tp].loc[df_a['quote_date'] == qd].copy()
+    if exp_mode_search == 'closest_next' or exp_mode_search == 'closest_previous':    # not exact expiration date
         if len(df) == 0:
             return -1, None
         df_exp = df.iloc[(df['expiration'] - exp).abs().argsort()]   # exact, before and after dates
-        if expiration_mode_search == 'n':     # next closest expiration date
+        if exp_mode_search == 'closest_next':     # next closest expiration date
             df_exp = df_exp.loc[df['expiration'] >= exp]    # filter dates before target
         if len(df) > 0:
             exp = df_exp['expiration'].iloc[0]
         else:
             return -2, None
+    elif not exp_mode_search == 'exact':
+        exit('wrong exp_mode_search. error 005')
     df = df.loc[df['expiration'] == exp]
     if len(df) == 0:
         return -3, None
-    if up and len(df) > 1:
-        df = df.iloc[(df['strike'] - strike).abs().argsort()][1:2]
-    else:
-        df = df.iloc[(df['strike'] - strike).abs().argsort()][0:1]
+    df_bk = df.copy()
+    if len(df) > 1:
+        if strike_round_mode == 'up':
+            df = df.iloc[(df['strike'] - strike).abs().argsort()][1:2]
+        elif strike_round_mode == 'down':
+            df = df.iloc[(df['strike'] - strike).abs().argsort()][0:1]
+        elif strike_round_mode == 'exact':
+            df = df.loc[df['strike'] == strike]
+        else:
+            exit('wrong strike_round mode. error 006')
     if len(df) == 0:
+        s_sort = df_bk.iloc[(df_bk['strike'] - strike).abs().argsort()]
+        mi = s_sort['strike'].iloc[-1]
+        ma = s_sort['strike'].iloc[0]
+
+        print(d2s(quote_date),'get_opt: looking for strike', strike,'at exp',d2s(expiration),'found,', mi,'-',ma)
         return -4, None
     ret_strike = df.iloc[0]['strike']
     if abs(ret_strike - strike) > max_delta:
--- read_cboe.py
+++ read_cboe.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from os import walk
-d = '../SPY_2020/'
+from au import s2d
+d = '../data/SPY_2020_CBOE_FILES/'
 i = 0
 f = []
 for (path, names, filenames) in walk(d):
@@ -11,14 +12,21 @@ f.sort()
 rf = None
 for fn in f:
     raw = pd.read_csv('%s/%s' % (d,fn))
-    cf = raw.loc[raw['option_type'] == 'P'].loc[raw['underlying_bid_1545'] - 15 < raw['strike']].loc[
-        raw['strike'] < raw['underlying_bid_1545'] + 15]
+    qd = raw['quote_date'].iloc[0]
+    if qd == '2020-03-11':
+        dev = 61
+    elif qd == '2020-06-15':
+        dev = 25
+    elif qd == '2020-08-17':
+        dev = 20
+    else:
+        dev = 15
+    cf = raw.loc[raw['option_type'] == 'P'].loc[raw['underlying_bid_1545'] - dev < raw['strike']].loc[
+        raw['strike'] < raw['underlying_bid_1545'] + dev]
     if i == 0:
         rf = cf.copy()
     else:
         rf = rf.append(cf.copy())
     i += 1
-    if i > 10:
-        break
-rf.reset_index(drop=True).to_csv('../out/f.csv')
+rf.reset_index(drop=True).to_csv('../data/SPY_CBOE_2020_PUT.csv')
 
