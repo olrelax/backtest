232d8e2408039c09c8282b58378ed7776df3ca3c
oleg <olrelax@gmail.com>
2022-03-05 21:21:52 +0300

    march5.2

--- backtest_fast.py
+++ backtest_fast.py
@@ -13,8 +13,12 @@ plot_under = None
 single_pos_len = 0
 algo = ''
 comm = 0
-premium_min = 0.03
-strike_loss_limit = 0
+premium_min = 100.0
+strike_loss_limit = -100
+ylim_bottom = -100
+ylim_top = 100
+
+
 def normalize(df):
     opt_min = df[['sum_1','sum_2','sum']].to_numpy().min()
     opt_max = df[['sum_1','sum_2','sum']].to_numpy().max()
@@ -37,13 +41,11 @@ def plot(df,types,sides,params):
         dfp = df[cols.split(',')]
     dfp = dfp.set_index('expiration')
     txt = '{}, type {}, side {},param {},lim max {},lim min {} '.format(algo,types,sides,params,premium_max,premium_min)
-    ax = dfp.plot(figsize=(10,7), title=txt)
+    ax = dfp.plot(figsize=(11,7), title=txt)
     xtick = pd.date_range(start=dfp.index.min(), end=dfp.index.max(), freq='M')
     ax.set_xticks(xtick, minor=True)
     ax.grid('on', which='minor')
     ax.grid('on', which='major')
-    ylim_bottom = -60
-    ylim_top = 80
     ax.set_ylim(ylim_bottom,ylim_top)
     plt.savefig('../out/c-%s.png' % fn)
 
@@ -61,8 +63,8 @@ def get_name(var):
 
 def save(dtf,arg_name=None):
     name = get_name(dtf)[0] if arg_name is None else arg_name
-    print('skip save',name)
-    # dtf.to_csv('../devel/%s.csv' % name,index=True)
+    print('save',name)
+    dtf.to_csv('../devel/%s.csv' % name,index=True)
 def get_sold(price):
     return 0 if price < 0.0101 else price
 
@@ -78,11 +80,11 @@ def get_df_for_side(param,side,opt_type):
     df_in = df.loc[df['days_to_exp'] > 0].copy()
     if algo == 'dist':
         dist = -param if opt_type == 'C' else param
-        df_in['delta'] = (df_in['underlying_bid_1545'].sub(dist).sub(df_in['strike']).abs()*10.0)
+        df_in['delta'] = (df_in['underlying_ask_1545'].sub(dist).sub(df_in['strike']).abs()*10.0)
     elif algo == 'disc':
         disc = -param if opt_type == 'C' else param
         k = (100-disc)/100
-        df_in['delta'] = (df_in['underlying_bid_1545']*k - df_in['strike']).abs() * 10.0
+        df_in['delta'] = (df_in['underlying_ask_1545']*k - df_in['strike']).abs() * 10.0
     elif algo == 'price':
         df_in['delta'] = df_in['bid_1545'].sub(param).abs()*100
     else:
@@ -92,13 +94,13 @@ def get_df_for_side(param,side,opt_type):
     s = s.to_frame()
     m = pd.merge(s,df_in,on=['expiration','delta']).sort_values(['quote_date','expiration']).drop_duplicates(subset=['quote_date','expiration'])
     o_out = df.loc[df['quote_date'] == df['expiration']]
-    o_out = o_out[['expiration','strike','underlying_bid_1545','ask_1545','bid_1545']]
+    o_out = o_out[['expiration','strike','underlying_bid_1545','underlying_ask_1545','ask_1545','bid_1545','ask_eod','bid_eod']]
     o = pd.merge(m,o_out, on=['expiration','strike'])
     if side == 'S':
-        o['profit'] = (o['bid_1545_x'] - pd.Series(map(get_sold,o['ask_1545_y']))) - comm
+        o['profit'] = (o['bid_1545_x'] - pd.Series(map(get_sold,o['ask_eod_y']))) - comm
     else:
-        o['profit'] = (pd.Series(map(get_sold,o['bid_1545_y'])) - o['ask_1545_x']) - comm
-    o = o[['quote_date','expiration','strike','underlying_bid_1545_x','bid_1545_x','ask_1545_x','exp_weekday','days_to_exp','underlying_bid_1545_y','bid_1545_y','ask_1545_y','profit']]
+        o['profit'] = (pd.Series(map(get_sold,o['bid_eod_y'])) - o['ask_1545_x']) - comm
+    o = o[['quote_date','expiration','strike','underlying_ask_1545_x','bid_1545_x','ask_1545_x','exp_weekday','days_to_exp','underlying_bid_1545_y','bid_1545_y','ask_1545_y','profit']]
     o.sort_values(['quote_date','expiration'])
     return o
 def read_opt_file(ofn):
@@ -109,9 +111,7 @@ def read_opt_file(ofn):
 def get_strike_loss(df):
     df_loss = df.loc[df['strike_0'] - df['under_in_0'] > strike_loss_limit].copy()
     df_loss['str_loss'] = df_loss['strike_0'] - df_loss['under_in_0']
-    save(df_loss,'df_loss_src')
     df_loss = df_loss.drop_duplicates(subset=['expiration'])
-    save(df_loss)
     return df_loss
 def get_df_between(df_in2exp,side,tp,i):
     side_sign = 1. if side == 'S' else -1.
@@ -132,7 +132,6 @@ def get_df_between(df_in2exp,side,tp,i):
     df_between = df_between[['quote_date','expiration','under_in_%d' % i,'strike_%d' % i,'bid_in_%d' % i,'ask_in_%d' % i,'under_out_%d' % i,'bid_out_%d' % i,'ask_out_%d' % i]]
     df_between['profit_%d' % i] = (df_between['bid_in_%d' % i] - df_between['ask_out_%d' % i]) * side_sign
     df_between = get_strike_loss(df_between)
-    save(df_between)
     return df_between
 
 
@@ -145,14 +144,13 @@ def backtest(types,sides,params):
     for i in range(count):
         print('get_in2exp')
         df_in2exp = get_df_for_side(params[i],sides[i],types[i])
-        df_in2exp = df_in2exp.rename(columns={'strike':'strike_%d' % i,'underlying_bid_1545_x':'under_in_%d' % i,'bid_1545_x':'bid_in_%d' % i,'ask_1545_x':'ask_in_%d' % i,'underlying_bid_1545_y':'under_out_%d' % i,'bid_1545_y':'bid_out_%d' % i,'ask_1545_y':'ask_out_%d' % i,'profit':'profit_%d' % i})
+        df_in2exp = df_in2exp.rename(columns={'strike':'strike_%d' % i,'underlying_ask_1545_x':'under_in_%d' % i,'bid_1545_x':'bid_in_%d' % i,'ask_1545_x':'ask_in_%d' % i,'underlying_bid_1545_y':'under_out_%d' % i,'bid_1545_y':'bid_out_%d' % i,'ask_1545_y':'ask_out_%d' % i,'profit':'profit_%d' % i})
         df_in2exp = df_in2exp[['quote_date','expiration','under_in_%d' % i,'strike_%d' % i,'bid_in_%d' % i,'ask_in_%d' % i,'under_out_%d' % i,'bid_out_%d' % i,'ask_out_%d' % i,'profit_%d' % i]]
         df_between = get_df_between(df_in2exp,sides[i],types[i],i)
         df_in2exp = df_in2exp.drop(columns={'pair_in'})
         df_cont = df_in2exp.append(df_between,ignore_index=True).sort_values(['expiration','quote_date'])
         df_cont = df_cont.drop_duplicates(subset=['expiration'],keep='last')      # drop expiration record if exit occurred before
 
-        save(df_cont)
         single_pos_len = df_cont.shape[1] - 1 if i == 0 else single_pos_len
         df = df_cont if i == 0 else pd.merge(df,df_cont,on='expiration')
     df = df.loc[(df['bid_in_0'] < premium_max) & (df['bid_in_0'] > premium_min)].copy()
@@ -175,13 +173,13 @@ def backtests():
     global before_date,draw_or_show, start_year,  premium_max,fn,plot_under,algo,premium_min,strike_loss_limit
     types = ['P','P']
     sides = ['S','L']
-    params = [5]
+    params = [8]
     algo = 'disc'
-    premium_max = 2.5
+    premium_max = 200.5
     premium_min = 0.00
-    strike_loss_limit = -10
+    strike_loss_limit = 100
     start_year = 2020
-    before_date = '2023-05-01'
+    before_date = '2021-01-01'
     plot_under = False
     draw_or_show = 'show'
     df = backtest(types,sides,params)
